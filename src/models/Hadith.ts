952afb1bc290794be6b4f56531cb3c83bb765a23
MKSherbini <mksherbini@gmail.com>
2024-06-13 22:51:19 +0300

    remove title and link from hadiths

--- src/models/Hadith.ts
+++ src/models/Hadith.ts
@@ -1,11 +1,9 @@
 export interface Hadith {
 	id: number;
-	title: string;
 	hadith_text: string;
 	explanation: string;
 	word_meanings?: string;
 	benefits: string;
 	grade: string;
 	takhrij: string;
-	link: string;
 }
